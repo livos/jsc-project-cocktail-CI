5848978c5883abf424e155e69e63fcada6c4ff56
livos <lvivancos2nd-dev@yahoo.fr>
2022-05-05 14:04:28 +0700

    last commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem est
+        officia, temporibus porro molestiae harum doloribus impedit pariatur
+        magnam repudiandae earum, adipisci asperiores necessitatibus
+        reprehenderit cumque maxime nostrum. Non harum labore ducimus rerum eum
+        qui recusandae, itaque consequatur cumque in sit at. Veniam, voluptates.
+      </p>
+    </section>
+  );
+};
+
+export default About;
